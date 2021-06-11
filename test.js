11909ecaa8c8df1e5ea3d90b619b4dc34c854031
Stephan Schinkel <schinkel.stephan@gmail.com>
2021-06-11 09:39:32 +0200

    fourth

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 console.log('test.js')
+var zummitIframeAdded = false;
 
 setInterval(function() {
 	var reactElem = document.getElementById('react');
@@ -6,6 +7,18 @@ setInterval(function() {
 	
 	if ( reactElem != null ) {
 		reactElem.style.height = "calc(100vh - 100px)";
+
+		if ( zummitIframeAdded == false ) {
+			var frameElem = document.createElement("iframe");
+			frameElem.src = "https://raw.githubusercontent.com/hacktick/hacktick.github.io/main/test.html"
+			frameElem.style.bottom = "0px";
+			frameElem.style.position = "fixed";
+			frameElem.style.width = "100%";
+	
+			document.body.appendChild(frameElem);
+
+			zummitIframeAdded = true;
+		}
 	}
 	if ( toolboxElem != null ) {
 		toolboxElem.style.bottom = "0";
