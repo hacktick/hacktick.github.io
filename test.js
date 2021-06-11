b6b25703aaca07c2ee9646c5e827587c7b0cb0cd
Stephan Schinkel <schinkel.stephan@gmail.com>
2021-06-11 07:24:06 +0200

    second

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
 console.log('test.js')
-document.getElementById('react').style.height = "calc(100vh - 150px)";
+
+setInterval(function() {
+	var reactElem = document.getElementById('react');
+	var toolboxElem = document.getElementById('new-toolbox');
+	
+	if ( reactElem != null ) {
+		reactElem.style.height = "calc(100vh - 100px)";
+	}
+	if ( toolboxElem != null ) {
+		toolboxElem.style.bottom = "0";
+	}
+}, 1000);
